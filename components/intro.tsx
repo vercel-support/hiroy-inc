acae4af4a42b4daf9681f81b7dad4a481f3b3d66
Roy Sivan <roy@roysivan.com>
2021-01-06 00:44:49 -0500

    adding club

--- components/intro.tsx
+++ components/intro.tsx
@@ -8,8 +8,8 @@ import { faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons';
 const Intro = () => {
   return (
     <>
-      <section className="flex flex-col items-center md:justify-between mt-16 mb-16">
-        <h1 className="flex-1 text-center text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+      <section className="flex flex-col items-center mt-16 mb-16">
+        <h1 className="flex-1 text-center text-8xl font-bold tracking-tighter leading-tight">
           Hi.
         </h1>
         <h4 className="flex-1 text-center text-lg mt-5">
@@ -23,8 +23,11 @@ const Intro = () => {
         <a href="https://github.com/royboy789" className="flex flex-row items-center mb-10" target="_blank" rel="noopener noreferrer">
           <FontAwesomeIcon icon={faGithub} className="w-6 mr-1" /> / @royboy789
         </a>
-        <a href="https://bit.ly/hi-roy" className="flex flex-row items-center" target="_blank" rel="noopener noreferrer">
+        <a href="https://bit.ly/hi-roy" className="flex flex-row items-center mb-2" target="_blank" rel="noopener noreferrer">
           swag @ https://bit.ly/hi-roy
+        </a>
+        <a href="https://hiroy.club" className="flex flex-row items-center" target="_blank" rel="noopener noreferrer">
+          club @ https://hiroy.club
         </a>
       </section>
     </>
