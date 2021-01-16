2cafcc3a92847dcc74df03dced9e903025301d5a
Roy Sivan <roy@roysivan.com>
2021-01-15 22:18:44 -0500

    adding personal image, removing some assets

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,9 +5,9 @@ const Footer = () => {
   return (
     <footer className="bg-accent-1 border-t border-accent-2">
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
+        <div className="py-10 flex flex-col lg:flex-row items-center">
           <span className="text-sm font-bold tracking-tighter leading-tight text-left">
-            &copy; 2021 Hi Roy LLC (not even joking)
+            &copy; 2021 Hi Roy INC (not even joking)
           </span>
         </div>
       </Container>
